9e4a9cbe5bfe94d233c15342daa052c742a7e3f9
Marcel <wagmarcel@web.de>
2023-01-24 17:30:00 +0100

    frontend: changed displayed field name for metrics

--- fusionfrontend/src/app/factory/components/content/metrics-board/metrics-board.component.ts
+++ fusionfrontend/src/app/factory/components/content/metrics-board/metrics-board.component.ts
@@ -24,6 +24,7 @@ import { environment } from '../../../../../environments/environment';
 import { takeUntil, tap } from 'rxjs/operators';
 import { NgsiLdService } from '../../../../core/services/api/ngsi-ld.service';
 import { ngsiLdLatestKeyValues } from '../../../../core/models/kairos.model';
+//import { DetailService } from '@clr/angular/data/datagrid/providers/detail.service';
 
 @Component({
   selector: 'app-metrics-board',
